48c419644c10a3774a183d54046b4a21877e4ca4
MAROUANE-ED-DAHRAOUI <dahraouimarouan@gmail.com>
2024-11-02 14:37:22 +0100

    Digit frequency

---
+++ level1/50_Problems/p8.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int read_number(string str)
+{
+    int nb = 0;
+
+    cout << str;
+    cin >> nb;
+    return (nb);
+}
+
+int count_digit_check(int nb, int freq)
+{
+    int cnt, remainder;
+
+    cnt = 0;
+    remainder = 0;
+    while(nb > 0)
+    {
+        remainder = nb % 10;
+        nb /= 10;
+        if(remainder == freq)
+            cnt++;
+    }
+    return (cnt);
+}
+
+void check_digit(int nb)
+{
+    int remainder, i;
+    
+    remainder = 0;
+    i = 0;
+    while(i < 10)
+    {
+        remainder = count_digit_check(nb, i);
+        if(remainder > 0)
+            cout << "Digit " << i << " Frequency is " << remainder << endl;
+        i++;
+    }
+}
+
+int main()
+{
+    int nb;
+
+    check_digit(read_number("pleas enter a positive Number : "));
+    cout << endl;
+    return 0;
+}
