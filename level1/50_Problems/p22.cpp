f17eaead8e935a92a60db7b27d082140eb321f5d
MAROUANE-ED-DAHRAOUI <dahraouimarouan@gmail.com>
2024-11-08 14:29:56 +0100

    Fill array with random numbers from 1 to 100

---
+++ level1/50_Problems/p22.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int read_number()
+{
+    int nb_elements;
+
+    cout << "Enter the number of elements : ";
+    cin >> nb_elements;
+    return (nb_elements);
+}
+
+int random_number(int from, int to)
+{
+    int nb;
+
+    nb = rand() % (to - from + 1) + from;
+    return (nb);
+}
+
+void    fill_array(int arr[100], int nb_elements)
+{
+    int i;
+
+    i = 0;
+    cout << "array elements : ";
+    while(i < nb_elements)
+    {
+        arr[i] = random_number(1, 100);
+        cout << arr[i] << " ";
+        i++;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+
+    fill_array(arr, read_number());
+    return 0;
+}
