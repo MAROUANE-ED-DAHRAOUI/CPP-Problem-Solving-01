b9cdd6f948b5882883feb227a87a21ded0da39e8
Marouane Ed Dahraoui <med-dahr@c2r5p10.1337.ma>
2024-11-09 19:02:28 +0100

    Average of all array

---
+++ level1/50_Problems/p26.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+using namespace std;
+
+int read_number()
+{
+    int nb_elements;
+
+    cout << "---------------------------------\nEnter the number of elements : ";
+    cin >> nb_elements;
+    cout << "-----------------------------------------------\n";
+    return (nb_elements);
+}
+
+int random_number(int from, int to)
+{
+    int nb;
+
+    nb = rand() % (to - from + 1) + from;
+    return (nb);
+}
+
+void    fill_array(int arr[100], int nb_elements)
+{
+    int i, sum;
+
+    i = 0, sum = 0;
+    cout << "array elements : ";
+    while(i < nb_elements)
+    {
+        arr[i] = random_number(1, 100);
+        cout << arr[i] << " ";
+        sum += arr[i]; 
+        i++;
+    }
+    cout << "\n----------------------------------------------\n";
+    cout << "Average of all numbers : " << (sum / nb_elements) << endl;
+    cout << "---------------------------\n";
+}
+
+int main()
+{
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+
+    fill_array(arr, read_number());
+    return 0;
+}
